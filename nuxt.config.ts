f83855a20ee0ccc7969d4277e5395e04ab3eeecc
Jackson Lynch <jacksonthatsme@gmail.com>
2024-03-04 01:44:45 -0500

    tried to fix static content rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  target: 'static',
   components: true,
   modules: [
     '@fullpage/nuxt-fullpage',
